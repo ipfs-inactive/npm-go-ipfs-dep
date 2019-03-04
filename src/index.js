fd3a59e9f22350e4e16c13e7bc7058bc60baeadc
Henrique Dias <hacdias@gmail.com>
2019-03-04 12:55:50 +0000

    feat: get dist info online
    License: MIT
    Signed-off-by: Henrique Dias <hacdias@gmail.com>

--- src/index.js
+++ src/index.js
@@ -25,94 +25,112 @@ const path = require('path')
 const request = require('request')
 const tarFS = require('tar-fs')
 const unzip = require('unzip-stream')
-const support = require('./check-support')
 const pkg = require('./../package.json')
 
-// Check package.json for default config
-const goIpfsInfo = pkg['go-ipfs']
-
-const goIpfsVersion = (goIpfsInfo && goIpfsInfo.version)
-  ? pkg['go-ipfs'].version
-  : 'v' + pkg.version.replace(/-[0-9]+/, '')
-
-let distUrl = (goIpfsInfo && goIpfsInfo.distUrl)
-  ? pkg['go-ipfs'].distUrl
-  : 'https://dist.ipfs.io'
-
-// Main function
-function download (version, platform, arch, installPath) {
+function unpack ({ url, installPath, stream }) {
   return new Promise((resolve, reject) => {
-    //            Environment Variables           Args        Defaults
-    version = process.env.TARGET_VERSION || version || goIpfsVersion
-    platform = process.env.TARGET_OS || platform || goenv.GOOS
-    arch = process.env.TARGET_ARCH || arch || goenv.GOARCH
-    distUrl = process.env.GO_IPFS_DIST_URL || distUrl
-    installPath = installPath ? path.resolve(installPath) : path.resolve(process.cwd())
-
-    // Make sure we support the requested package
-    try {
-      support.verify(version, platform, arch)
-    } catch (e) {
-      return reject(e)
+    if (url.endsWith('.zip')) {
+      return stream.pipe(
+        unzip
+          .Extract({ path: installPath })
+          .on('close', resolve)
+          .on('error', reject)
+      )
     }
 
-    // Flag for Windows
-    const isWindows = support.isWindows(platform)
-
-    // Create the download url
-    const fileExtension = isWindows ? '.zip' : '.tar.gz'
-    const fileName = 'ipfs_' + version + '_' + platform + '-' + arch + fileExtension
-    const url = distUrl + '/go-ipfs/' + version + '/go-' + fileName
-
-    // Success callback wrapper
-    // go-ipfs contents are in 'go-ipfs/', so append that to the path
-    const done = () => resolve({
-      fileName: fileName,
-      installPath: path.join(installPath, '/go-ipfs/')
-    })
-
-    // Unpack the response stream
-    const unpack = (stream) => {
-      // TODO: handle errors for both cases
-      if (isWindows) {
-        return stream.pipe(
-          unzip
-            .Extract({ path: installPath })
-            .on('close', done)
-        )
-      }
-
-      return stream
-        .pipe(gunzip())
-        .pipe(
-          tarFS
-            .extract(installPath)
-            .on('finish', done)
-        )
-    }
-
-    // Start
-    process.stdout.write(`Downloading ${url}\n`)
-
-    request.get(url, (err, res, body) => {
-      if (err) {
-        // TODO handle error: haad?
-        return reject(err)
-      }
-      // Handle errors
-      if (res.statusCode !== 200) {
-        reject(new Error(`${res.statusCode} - ${res.body}`))
-      }
-    })
-      .on('response', (res) => {
-      // Unpack only if the request was successful
-        if (res.statusCode !== 200) {
-          return
-        }
-
-        unpack(res)
-      })
+    return stream
+      .pipe(gunzip())
+      .pipe(
+        tarFS
+          .extract(installPath)
+          .on('finish', resolve)
+          .on('error', reject)
+      )
   })
 }
 
-module.exports = download
+function download ({ installPath, url }) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, res, body) => {
+      if (err) return reject(err)
+      if (res.statusCode !== 200) reject(new Error(`${res.statusCode} - ${res.body}`))
+    }).on('response', (res) => {
+      if (res.statusCode !== 200) return
+
+      unpack({ url, installPath, stream: res })
+        .then(resolve)
+        .catch(reject)
+    })
+  })
+}
+
+function cleanArguments (version, platform, arch, installPath) {
+  const goIpfsInfo = pkg['go-ipfs']
+
+  const goIpfsVersion = (goIpfsInfo && goIpfsInfo.version)
+    ? pkg['go-ipfs'].version
+    : 'v' + pkg.version.replace(/-[0-9]+/, '')
+
+  const distUrl = (goIpfsInfo && goIpfsInfo.distUrl)
+    ? pkg['go-ipfs'].distUrl
+    : 'https://dist.ipfs.io'
+
+  return {
+    version: process.env.TARGET_VERSION || version || goIpfsVersion,
+    platform: process.env.TARGET_OS || platform || goenv.GOOS,
+    arch: process.env.TARGET_ARCH || arch || goenv.GOARCH,
+    distUrl: process.env.GO_IPFS_DIST_URL || distUrl,
+    installPath: installPath ? path.resolve(installPath) : path.resolve(process.cwd())
+  }
+}
+
+function get (url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, res, body) => {
+      if (err) return reject(err)
+      if (res.statusCode !== 200) return reject(new Error(`${res.statusCode} - ${res.body}`))
+      resolve(body)
+    })
+  })
+}
+
+async function ensureVersion ({ version, distUrl }) {
+  const res = await get(`${distUrl}/go-ipfs/versions`)
+  const versions = res.split('\n').filter(el => el !== '')
+
+  if (versions.indexOf(version) === -1) {
+    throw new Error(`Version '${version}' not available`)
+  }
+}
+
+async function getURL ({ version, platform, arch, distUrl }) {
+  await ensureVersion({ version, distUrl })
+
+  const dist = await get(`${distUrl}/go-ipfs/${version}/dist.json`)
+  const data = JSON.parse(dist)
+
+  if (!data.platforms[platform]) {
+    throw new Error(`No binary available for platform '${platform}'`)
+  }
+
+  if (!data.platforms[platform].archs[arch]) {
+    throw new Error(`No binary available for arch '${arch}'`)
+  }
+
+  const link = data.platforms[platform].archs[arch].link
+  return `${distUrl}/go-ipfs/${version}${link}`
+}
+
+module.exports = async function () {
+  const args = cleanArguments(...arguments)
+  const url = await getURL(args)
+
+  process.stdout.write(`Downloading ${url}\n`)
+
+  await download({ ...args, url })
+
+  return {
+    fileName: url.split('/').pop(),
+    installPath: path.join(args.installPath, '/go-ipfs/')
+  }
+}
